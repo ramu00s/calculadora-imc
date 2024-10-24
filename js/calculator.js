0151fc19ba624c18461841d78ae54473add2b38b
Ramon <rgomessilva16@gmail.com>
2024-10-23 23:01:36 -0300

    Fiz a adissão da tabela com os valores de IMC e sua descrição, bem como adicionei um Switch para trazer pra tela do usuário o a descrição do IMC dele e, por fim, fiz alteração do bg da pagina Html, deixando o fundo geral light ficando apenas o título com o bg primary.

--- js/calculator.js
+++ js/calculator.js
@@ -14,8 +14,29 @@ function calculateImc(){
     //Onde será calculado o valor do IMC e a mensagem que será exibida para o usuário
     if (!isNaN(weight) && !isNaN(height) && height > 0) {
         let imc = weight / (height * height);
-        // Mensagem explicativa
+        //Mensagem explicativa
         let mensagem = `Seu IMC é <strong>${imc.toFixed(2)}</strong>.`;
+
+        //Irei adicionar nesse switch uma mensagem que mostrará a classificação do IMC do usuário
+        switch (true){
+            case(imc < 18.5):
+                mensagem += `\nVocê está <strong>abaixo do peso</strong> (Magreza).`;
+                break;
+            case(imc >= 18.5 && imc <= 24.9):
+                mensagem += `\nVocê está com o <strong>peso normal</strong>.`;
+                break;
+            case(imc >= 25 && imc <= 29.9):
+                mensagem += `\nVocê está com <strong>sobrepeso</strong>.`;
+                break;
+            case(imc >= 30 && imc <= 39.9):
+                mensagem += `\nVocê está com <strong>obesidade</strong> (Grau II).`;
+                break;
+            case(imc > 40):
+                mensagem += `\nVocê está com <strong>obesidade grave</strong> (Grau III).`;
+                break;
+            default:
+                mensagem += `\nNão foi possível classificar o IMC.`
+        }
         
         document.getElementById("result").innerHTML = mensagem;
     } else {
